220ed85008100524344a67d9fa7181a15493c492
Giuseppe Setem <giuseppe7d@gmail.com>
2020-07-28 10:52:27 +0100

    feat: implements commitlint and file structure

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+interface Props {}
+
+export default function Button(props: Props) {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+}
---
+++ src/components/Input/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+interface Props {}
+
+export default function Input(props: Props) {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+}
