3b7586f4dd73d6e533001f8244f62f8f4f8099b9
Giuseppe Setem <giuseppe7d@gmail.com>
2020-12-22 02:42:06 +0000

    feat(analyis): add 3d webview render

---
+++ src/components/containers/AnalysisCanvas/WebViewCanvas.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { View, Text } from "react-native";
+import Constants from "expo-constants";
+import WebView from "react-native-webview";
+
+const { RENDER_URL } = Constants.manifest.extra;
+
+interface Props {
+  folder?: string;
+  file?: string;
+}
+
+export const WebViewCanvas: React.FC<Props> = ({
+  folder = "instructions",
+  file = "abef9d6a-2497-4a37-895f-c6fa3ff16087.glb",
+}) => {
+  return (
+    <WebView
+      style={{ flex: 1, flexGrow: 1 }}
+      source={{ uri: `${RENDER_URL}/render/${folder}/${file}` }}
+      cacheMode="LOAD_CACHE_ELSE_NETWORK"
+      cacheEnabled
+      javaScriptEnabled
+      domStorageEnabled
+      thirdPartyCookiesEnabled
+      sharedCookiesEnabled
+    />
+  );
+};
--- src/components/containers/AnalysisCanvas/index.tsx
+++ src/components/containers/AnalysisCanvas/index.tsx
@@ -8,7 +8,7 @@ import { useTestBench } from "../../../hooks/useTestbench";
 import { useStores } from "../../../hooks/useStores";
 import { AnalysisInformation } from "../../fragments/AnalysisInformation";
 import { Wrapper, HeaderWrapper, CanvasIconButton } from "./styles";
-import { ThreeCanvas } from "./ThreeCanvas";
+import { WebViewCanvas } from "./WebViewCanvas";
 
 interface IProps {
   testBenchId: string;
@@ -44,25 +44,31 @@ export const AnalysisCanvas: React.FC<IProps> = observer(({ testBenchId }) => {
             )
           }
         >
-          <Icon name="chevron-left" size={24} />
+          <Icon name="box" size={24} />
         </CanvasIconButton>
       </HeaderWrapper>
-      {applicationStore.canvasMode === "photo" ? (
-        analysisStore.selectedInstruction && (
-          <Image
-            source={{ uri: analysisStore.selectedInstruction.sources[0].src }}
-            style={{
-              width: "100%",
-              height: "100%",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-            resizeMode="contain"
-          />
-        )
-      ) : (
-        <ThreeCanvas />
-      )}
+      {applicationStore.canvasMode === "photo"
+        ? analysisStore.selectedInstruction && (
+            <Image
+              source={{ uri: analysisStore.selectedInstruction.sources[0].src }}
+              style={{
+                width: "100%",
+                height: "100%",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+              resizeMode="contain"
+            />
+          )
+        : analysisStore.selectedInstruction && (
+            <WebViewCanvas
+              file={
+                analysisStore.selectedInstruction.step % 2 === 0
+                  ? "dcf67589-2f20-4f5c-a4a3-f8ad6b13a34e.glb"
+                  : "4581c22b-204c-410a-8bc9-9888f409c337.glb"
+              }
+            />
+          )}
       <AnalysisInformation
         items={[
           {
